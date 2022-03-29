3d32ec68e636a6075056d4d923b2787ea6672f8a
Felix Steinle <steinle@westphalia-datalab.com>
2022-03-29 15:02:05 +0200

    implement local & dev env

--- api/main.py
+++ api/main.py
@@ -4,7 +4,8 @@ from fastapi import FastAPI
 
 from core.models.database import engine, income_table
 from core.schemas.schema import income_data
-
+import uvicorn
+import os
 app = FastAPI()
 
 @app.get("/get_income_data")
@@ -13,3 +14,9 @@ async def get_income_data() -> income_data:
         query = session.query(income_table)
 
     return pd.read_sql(query.statement, query.session.bind)
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ["SERVER_PORT"]))
+
+
