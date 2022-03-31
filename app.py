f5c44c490f5e7d557f8c3c77bee93007e7c022ed
Felix Steinle <steinle@westphalia-datalab.com>
2022-03-31 18:11:50 +0200

    update frontend

--- app.py
+++ app.py
@@ -2,15 +2,35 @@
 import streamlit as st
 import requests
 import pandas as pd
+import numpy as np
 
-# Add title to the page.
-st.title("DASHBOARD")
+companies = ['IBM', 'Allianz']
 
-if st.button(label='Select Data'):
-    r = requests.get("https://gentle-caverns-79193.herokuapp.com/get_income_data")
-    data = pd.DataFrame(r.json())
-    st.dataframe(data)
-    st.write("Data select successfully!")
+with st.sidebar:
+    st.title("DASHBOARD")
+
+    add_selectbox = st.sidebar.multiselect(
+        "Please choose a company", companies
+    )
+
+    if st.button(label='Select Data'):
+        r = requests.get("https://gentle-caverns-79193.herokuapp.com/get_income_data")
+        data = pd.DataFrame(r.json())
+        data = data[['ebit']]
+
+columns = st.columns([3,1])
+
+with columns[0]:
+    st.header("Visualization")
+    data_viz = np.random.randn(10, 1)
+    st.line_chart(data_viz)
+
+with columns[1]:
+    st.header("Data")
+    try:
+        st.dataframe(data)
+    except:
+        print("Please select a company")
 
 
 # if __name__ == '__main__':
