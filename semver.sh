0c3544cd5c686d23239de7b9b9c00026e20fd66a
Felix Steinle <felix.steinle@publicissapient.com>
2024-03-27 11:40:18 +0100

    fix: init cz

--- semver.sh
+++ semver.sh
@@ -1,8 +1,8 @@
  #!/bin/sh
 
 echo 'Versioning the application'
-'exists=`git show-ref refs/heads/test_cicd` && if [ -n "$exists" ]; then git branch -D test_cicd; fi'
-git checkout -b test_cicd
+'exists=`git show-ref refs/heads/test_cicd_3` && if [ -n "$exists" ]; then git branch -D test_cicd_3; fi'
+git checkout -b test_cicd_3
 cz bump --yes # execute auto bump and push to master
 TAG=$(head -n 1 VERSION) # get the new software version and save into artifacts
 echo "#!/bin/sh" >> variables
